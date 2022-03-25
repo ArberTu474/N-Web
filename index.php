c31466dff68b266b323d9c07047db182ffd24bd7
ArberTu474 <arber.tusha@outlook.com>
2022-03-25 21:43:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Kafshet Shtepiake</title>
+  </head>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: sans-serif, Arial, Helvetica;
+    }
+
+    .container {
+      margin: 0 auto;
+      max-width: 800px;
+      padding: 0 30px;
+    }
+
+    .title {
+      font-size: 40px;
+      margin: 50px 0 20px 0;
+      border-left: solid 7px #f77a9b;
+      padding-left: 10px;
+    }
+
+    .title2 {
+      font-size: 30px;
+      margin: 50px 0 20px 0;
+      border-left: solid 7px #f77a9b;
+      padding-left: 10px;
+    }
+
+    .text {
+      font-size: 18px;
+      line-height: 26px;
+    }
+
+    img {
+      margin: 20px 0 70px 0;
+      border-radius: 20px;
+      width:  70%;
+      padding-left: 10px;
+    }
+
+    @media screen and (max-width: 600px) {
+      img {
+        width: 100%;
+      }
+
+      .title {
+        font-size: 33px;
+      }
+
+      .title2 {
+        font-size: 27px;
+      }
+    }
+  </style>
+<body>
+  <div class="container">
+    <h1 class="title">Pse kafshet shtepiake jane te rendesishme per njerezimin?</h1>
+    <p class="text">Une mendoj se kafshet shtepiake jane te rendesishme per njerezimin perndryshe nuk do ta kishin emrin kafshe "shtepiake".keto kafshe jane te destinuara per te ndenjur prane njerzimit por ndoshta mendimi juaj eshte ndryshe ndoshta ju mendoni. kafshet jane te rendesishme sepse ato na qendrojne gjithmone prane. ato na qendrojne prane kur ne jemi te hidheruar te trishtuar gjithashtu edhe kur jami te gezuar.nje arsye tjeter pse ato jane te rendesishme eshte sepse ne mund ti themi atyre sekretet tona pa pasur friken se "bota" i zbulon.nje arsye tjeter eshte se ato i falin dashuri cdo njeriu sidomos atyre qe "zoti nuk ia ka falur nje femije per kete arsye kafshet mund ti japin dashurine qe ketyre njerzve i mungon.arsyeja e fundit por jo me pak e rendesishme eshte se ato na ndihmojne ne jeten e perditshme.
+    me keto argumente shpresoj tju kem bindur ti doni kafshet sepse ato jane te vetmit gjallesa qe do tju dan pa kushte.</p>
+    
+    <?php 
+      echo "<h2 class='title2'>Kafsha me e preferuar nga njerezit sot</h1>
+      <p class='text'>Qente jane besnike dhe miqesore me qeniet njerezore me shume mundesi per shkak te gjeneve te tyre, thote nje studim i ri i botuar në revisten Science Advances. Nje ekip shkencetaresh nga Indiana ne Shtetet e Bashkuara gjeten variacione ne disa gjene qensh qe i bejne ata me miqesore se ujqerit, nga te cilet rrjedhin.</p>"
+      ?>
+      <img src="https://i.ibb.co/fnCkYnG/68-AB9-FD8-ADAB-48-E1-A950-1-D8394870-A82.jpg"/>
+    </body>
+  </div>
+
+<script>
+  console.log("hello");
+</script>
+</html>
